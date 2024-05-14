e2efdb0c62e89869434350cf51d7258918f7daf6
Aron Harsfalvi <aron.harsfalvi@gmail.com>
2024-05-14 16:50:28 +0200

    Initial commit with empty next js project with bootstrap

---
+++ src/app/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return <div className="container">
+        <h1 className="text-center mt-5">Welcome to My Next.js App</h1>
+        <p className="lead text-center">This is a simple Next.js app with Bootstrap.</p>
+        <div className="d-flex justify-content-center">
+            <button className="btn btn-primary">Click Me</button>
+        </div>
+    </div>
+}
