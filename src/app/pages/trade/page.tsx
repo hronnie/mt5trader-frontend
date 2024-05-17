1d2119dbaa25c28394b1b784d4ef229fa158b4e7
Aron Harsfalvi <aron.harsfalvi@gmail.com>
2024-05-17 15:29:26 +0200

    Fixes react-bootstrap and adds 2 input

--- src/app/pages/trade/page.tsx
+++ src/app/pages/trade/page.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+"use client"
+import React, {useState} from 'react';
+import CurrencyDropdown from "@/app/shared/components/CurrencyDropdown";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { Form } from 'react-bootstrap';
+import NumberInput from "@/app/shared/components/NumberInput";
 
 const Trade: React.FC = () => {
+    const [selectedCurrency, setSelectedCurrency] = useState<string>("");
+    const [numberValue, setNumberValue] = useState<number>(0);
+
+    const handleCurrencySelect = (currency: string) => {
+        setSelectedCurrency(currency);
+        console.log(`Parent component received selected currency: ${currency}`);
+    };
+
+    const handleNumberChange = (value: number) => {
+        setNumberValue(value);
+        console.log(`Parent component received number value: ${value}`);
+    };
+
     return (
         <div>
             <h1>Trade</h1>
-            <p>Welcome to the Trade page.</p>
+            <Card style={{ width: '18rem' }}>
+                <Card.Title>Choose currency</Card.Title>
+                <Card.Body>
+                    <CurrencyDropdown onCurrencySelect={handleCurrencySelect}/>
+                    <NumberInput value={numberValue} onChange={handleNumberChange} />
+                </Card.Body>
+            </Card>
         </div>
     );
 };
